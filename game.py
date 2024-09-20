c1f563dcf15d475c1f1fb1ed0972f536071c8480
James Nemeno <nemenojames@gmail.com>
2024-09-19 19:14:07 -0800

    initial commit

---
+++ game.py
@@ -0,0 +1,12 @@
+'''
+Ultimate Tic Tac Toe is a version of Tic Tac Toe showcased by VSauce each tile has its own separate Tic Tac Toe game
+and when you choose a box. The next player must choose a move in the game that corresponds to the move. When a player
+wins one of the games, that game turns into an X or O depending on the player's side.
+'''
+
+import  as p
+import ticTacToe
+
+def main():
+    pass
+
